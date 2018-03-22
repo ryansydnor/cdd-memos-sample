c4cdf92d56bddfec49022839ec532469a4d987ca
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2018-03-22 08:54:22 -0400

    Add UserDetails to MemoLine

--- src/ComponentDriven/MemoLineContainer.js
+++ src/ComponentDriven/MemoLineContainer.js
@@ -2,15 +2,17 @@ import React from 'react'
 import Typography from 'material-ui/Typography';
 import { withApollo } from 'react-apollo';
 import { MEMO_FRAGMENT } from '../graphql/graphql';
+import UserDetailsContainer from './UserDetailsContainer';
 import './MemoLine.css';
 
-export const MemoLineLayout = ({ text }) => (
+export const MemoLineLayout = ({ text, user: { id } }) => (
   <div className="MemoLine">
     <div className="MemoLine__checkbox">
       <Typography>
         { text }
       </Typography>
     </div>
+    <UserDetailsContainer id={id} />
   </div>
 )
 
